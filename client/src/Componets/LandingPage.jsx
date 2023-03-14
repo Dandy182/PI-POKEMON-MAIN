70a39a47def4332175735dc2e8b6a724a0501da3
Daniel Yesid Cely Robayo <danielcely1@gmail.com>
2023-03-13 23:48:19 -0500

    avance front-end

--- client/src/Componets/LandingPage.jsx
+++ client/src/Componets/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import pokeball from '../img/Poké_Ball_icon.svg';
 //import { Link } from "react-router-dom";
-
+import '../index.css'
 
 export default function Landing(){
 
 
   return(<div className="landing__Page">
+    <img src={pokeball} className="pokeball" alt="pokeball" />
     <button className="btn btn-start">Start</button>
   </div>)
 }
