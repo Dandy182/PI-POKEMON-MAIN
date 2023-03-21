b650fa8933c969496a9f8b4daa15298034d2282a
Daniel Yesid Cely Robayo <danielcely1@gmail.com>
2023-03-21 01:19:56 -0500

    avance usando la primera ruta

---
+++ api/controllers/getPokemons.js
@@ -0,0 +1,13 @@
+
+const axios = require('axios');
+
+
+
+ const getPokemons = (req, res) => {
+  res.send('Hola Mundo')
+};
+
+
+module.exports = {
+  getPokemons
+}
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const axios = require('axios');
 
 
 const router = Router();
@@ -8,6 +9,30 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+const getDataApi = async () =>{
+  const pokemon = [];
+  
+  const getApi = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=10000`);
+  const dataAPi = await getApi.data.results.map(p => {
+    return {
+      pkm: p.url
+    }
+  })
 
+  return dataAPi;
+
+}
+
+
+router.get('/pokemons', async (req, res) =>{
+  const datos = await getDataApi();
+  const pokemon = await dataPokemon(datos);
+  res.json(pokemon);
+
+});
+// router.get('/pokemons/:idpokemon', getPokemonsId);
+// router.get('/pokemons/name?', getPokemonsQuery);
+// router.get('/types', getTypes);
+// router.post('/pokemons', addPokemon)
 
 module.exports = router;
