42e90d076dcfbd6264b05f2af30af3bb7dce897b
Daniel Yesid Cely Robayo <danielcely1@gmail.com>
2023-03-28 00:25:56 -0500

    ruta 404 realizada

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Landing from './Componets/LandingPage';
 import Home from './Componets/Home';
+import Error from './Componets/404';
 import '../src/index.css'
 
 
@@ -14,7 +15,8 @@ function App() {
       <div className="App">
         <Switch>
           <Route exact path="/"><Landing /></Route>
-          <Route exact path='/home'><Home /></Route> 
+          <Route exact path="/home"><Home /></Route>
+          <Route path="*" component={Error} />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ client/src/Componets/404.jsx
@@ -0,0 +1,10 @@
+import '../css/error.css';
+
+
+export default function Error(){
+
+  return(<div className="backgroundError">
+    <h1>Un Pokémon dormido bloquea el camino !!!</h1>
+    <h1>Error 404</h1>
+  </div>)
+}
