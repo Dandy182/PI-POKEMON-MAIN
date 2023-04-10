4df3e38f50069791394111ce98af3b026a914c3d
Daniel Yesid Cely Robayo <danielcely1@gmail.com>
2023-04-10 09:22:50 -0500

    listo logica del detail

--- client/src/Componets/Detail.jsx
+++ client/src/Componets/Detail.jsx
@@ -22,9 +22,26 @@ export default function PokemonDetail ({match}){
         <UpperBar />
 
         <div className="contenedor contenido__cards">
-            <div className='detail'>
+                {
+                    pokemon.length > 0 ? (<div className='pokemonDetail'>
+                        <h1>{pokemon[0].name}</h1>
+                        <div className='detailsData'>
+                            <img src={pokemon[0].img} alt={pokemon[0].name} />
+                            <div className='detailsInfo'>
+                                <span>life: </span><p>{pokemon[0].hp}</p>
+                                <span>attack: </span><p>{pokemon[0].atk}</p>
+                                <span>defense: </span><p>{pokemon[0].atk}</p>
+                                <span>speed: </span><p>{pokemon[0].speed}</p>
+                                <span>height: </span><p>{pokemon[0].height}</p>
+                                <span>weight: </span><p>{pokemon[0].weight}</p>
+                                <span>type:</span>{pokemon[0].type ? pokemon[0].type.map((t, index) => <p key={index}>{t}</p>): pokemon[0].types.map((t, index) => <p key={index}>{t}</p>) }
+                                
+                            </div>
+                        </div>
+
+                    </div>) : <p className='charge'>Loading ...</p>
+                } 
                 
-            </div>
         </div>
         </div>)
 }
