ce708907dc3f14ef00147f1013721bb8998520af
Daniel Yesid Cely Robayo <danielcely1@gmail.com>
2023-04-10 08:42:17 -0500

    Avance detail

--- client/src/Componets/Detail.jsx
+++ client/src/Componets/Detail.jsx
@@ -3,38 +3,27 @@ import '../css/detail.css'
 import UpperBar from "./upperBar"
 import { useDispatch, useSelector } from 'react-redux'
 import { getPokemonById } from '../actions';
+import { useParams } from 'react-router-dom';
 
 
 
-export default function PokemonDetail (props){
+export default function PokemonDetail ({match}){
 
+    const { id } = useParams();
     const dispatch = useDispatch()
     const pokemon = useSelector((state) => state.detail)
-
-    useEffect(() =>dispatch(getPokemonById(props.match.params.id)),[dispatch])
-
+    console.log(pokemon)
+    
+    useEffect(()=>{
+        dispatch(getPokemonById(id));
+    },[dispatch]);
 
     return(<div className='pokemon__detail'>
         <UpperBar />
 
         <div className="contenedor contenido__cards">
             <div className='detail'>
-               <h1>{pokemon[0].name}</h1>
-               <div>
-
-                <div className='img__pokemon'>
-                    <img src={pokemon[0].img} alt={pokemon[0].name}/>
-                </div>
-
-                <div className='pokemon__data'>
-                    <div>
-                        <span>Name: </span> <p>{pokemon[0].name}</p>
-                    </div>
-                    <div>
-                        <span>life: </span> <p>{pokemon[0].hp}</p>
-                    </div>
-                </div>
-               </div>
+                
             </div>
         </div>
         </div>)
