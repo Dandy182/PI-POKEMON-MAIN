73330856fd49e1a65ba70e4569405b6951f3a589
Daniel Yesid Cely Robayo <danielcely1@gmail.com>
2023-07-31 14:28:35 -0500

    revision poke api

--- api/src/controllers/pokemons.js
+++ api/src/controllers/pokemons.js
@@ -1,87 +1,39 @@
 const axios = require('axios')
-const {Pokemon, Type} = require('../db');
-
-const getDataApi = async () =>{
-
-  const allPokemons = [];
-  
-  try{
-  
-  const callApi = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=250`);
-  const urlsApi = await callApi.data.results.map(p => p.url);
-
-  await axios.all(urlsApi.map(urlApi => axios.get(urlApi)))
-    .then(pokemonsFound => {
-      pokemonsFound.map(pf => allPokemons.push({
-              id:pf.data.id,
-              name:pf.data.name,
-              img:pf.data.sprites.other['official-artwork'].front_default,
-              hp:pf.data.stats[0].base_stat,
-              atk:pf.data.stats[1].base_stat,
-              def:pf.data.stats[2].base_stat,
-              speed:pf.data.stats[5].base_stat,
-              height:pf.data.height,
-              weight:pf.data.weight,
-              createInDb:false,
-              type:pf.data.types.map((type) => type.type.name)
-      }));
-    },);
-
-    return allPokemons;
-
-  }catch(error){
-    console.log(error)
-  }
-
-}
-
-// const dataDB = async () => {
-//   const dataApi = await getDataApi()
-//   await dataApi.forEach(p => {
-//     Pokemon.findOrCreate({
-//       where:{name:p.name},
-//       defaults:{
-//         name:p.name,
-//         img:p.img,
-//         hp:p.hp,
-//         atk:p.atk,
-//         def:p.def,
-//         speed:p.speed,
-//         height:p.height,
-//         weight:p.weight,
-//       }
-//     })
+const {} = require('../db');
 
 
-//   })
-// }
 
-const dataDB = async () =>{
-  return await Pokemon.findAll({
-    attributes:{ exclude:["createdAt", "updatedAt"]},
-    include:{
-      model:Type,
-      attributes:['name'],
-      through:{
-        attributes:[],
-      }
+
+
+const  getDataApi = async () => {
+    const pokemons = [];
+
+    try{
+        const api = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=250`)
+        const dataPokemon = await api.data.results.map(p => p.url); 
+        
+        await axios.all(dataPokemon.map(pApi => axios.get(pApi)))
+            .then(pokemonsFound => { pokemonsFound.map(
+                pkmns => { 
+                    return pokemons.push({
+                    id:pkmns.data.id,
+                    name:pkmns.data.name
+                });
+
+            })
+            })
+        return pokemons;
+
+    }catch(error){
+        console.log(error)
     }
-  })
 }
 
 
-const getAllPokemons = async () =>{
-  const pokemonApi = await getDataApi();
-  const pokemonDb = await dataDB();
-  const getAll = pokemonApi.concat(pokemonDb);
-  
-  return getAll;
-}
+
 
 
 
 module.exports = {
-  getDataApi,
-  dataDB,
-  getAllPokemons
+    getDataApi
 }
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,8 @@
 const { Router } = require('express');
+const { getDataApi } = require('../controllers/pokemons');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const {dataDB, getAllPokemons} = require('../controllers/pokemons');
-const { getTypes } = require('../controllers/type');
-const {Pokemon, Type} = require('../db');
+
 
 
 
@@ -12,111 +11,16 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.get('/pokemons', async (req, res) =>{
- 
-  try{
-    // await dataDB();
-    // await getTypes();
-    const data = await getAllPokemons()
-    await getTypes();
-    
-    res.status(200).json(data)
 
-  }catch(error){
-    console.log(error)
-  }
+router.get('/pokemon', async (req, res)=>{
 
-})
+    try{
 
-router.get('/pokemons/name', async (req, res) =>{
-  const {name} = req.query;
-
-  try{
-    // await dataDB();
-    // await getTypes();
-    const data = await getAllPokemons()
-
-    // if(name){
-      let toSearch = name.toLowerCase();
-
-      // const pokemonFound = await Pokemon.findAll({
-      //   where:{
-      //     name:toSearch
-      //   },
-      //   attributes:{
-      //     exclude:["createdAt", "updatedAd"],
-      //   },
-      //   include:{
-      //     model:Type,
-      //     attributes:['name'],
-      //     through:{
-      //       attributes:[]
-      //     }
-      //   }
-      // })
-
-      let pokemonFound = await data.filter(p => p.name.toLowerCase().includes(toSearch))
-      pokemonFound.length ? res.status(200).json(pokemonFound) : res.status(404).json(`Pokémon with name: ${toSearch} not found`)
-    
-    // }else{
-    //   res.status(200).json(data);
-    // }
-
-  }catch(error){
-    console.log(error)
-  }
-
-})
-
-router.get('/pokemons/:id', async (req, res) => {
-  let id = req.params.id;
-
-  //console.log(id)
-  try{
-    const data =  await getAllPokemons();
-    pokemonFound = await data.filter(p => p.id == id)
-      pokemonFound.length ? res.status(200).json(pokemonFound) : res.status(404).json(`Pokémon with id: ${id} do not exist`)
-
-  }catch(error){
-    console.log(error);
-  }
-
-})
-
-router.post('/pokemons', async (req, res) =>{
-  let { name, img, hp, atk, def, speed, height, weight, type} = req.body; 
-
-  try{
-    let newPokemon = await Pokemon.create({
-      name,
-      img,
-      hp,
-      atk,
-      def,
-      speed,
-      height,
-      createInDb:true,
-      weight,
-    })
-
-    let typeDb = await Type.findAll({
-      where:{
-        name: type
-      }
-    })
-
-    newPokemon.addType(typeDb)
-
-    res.status(200).send(`Pokémon Created`)
-
-  }catch(error){
-    console.log(error);
-  }
-  })
-
-router.get('/types', async (req, res) =>{
-  const type = await getTypes()
-  res.status(200).send(type);
+        const data = getDataApi()
+        res.status(200).json(data)
+    }catch(error){
+        console.log(error)
+    }
 })
 
 
